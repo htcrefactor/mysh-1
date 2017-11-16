62c5636adad52cedeeca46e970c0042160b0bbd6
htcrefactor <htc.refactor@gmail.com>
2017-11-17 00:28:41 +0900

    Update commands.h

--- include/commands.h
+++ include/commands.h
@@ -7,7 +7,19 @@ struct single_command
   char** argv;
 };
 
+struct background{
+  int pid;
+  char *argv;
+  int flag;
+}bg;
+
 int evaluate_command(int n_commands, struct single_command (*commands)[512]);
+
+int exec_commands(struct single_command *com);
+
+void *background_thread(void *argv);
+
+void *socket_thread(void *argv);
 
 void free_commands(int n_commands, struct single_command (*commands)[512]);
 
